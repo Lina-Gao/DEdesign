8fb283c95f6cba9533432bdd44052759e3c73a64
Lina Gao <gao@ohsu.edu>
2018-04-05 16:22:15 -0700

    first version, single chip design

---
+++ R/gen.design.R
@@ -0,0 +1,114 @@
+#' Statistical design for RNA-seq experiments aimed at identifying differentially expressed genes
+#'
+#' Generates block design to assign samples to sequencing lanes and adaptors on an Illumina flow cell
+#'
+#'
+#' @param trts a character vector for treatment groups
+#' @param reps an integer vector for number of replicates for each element of \code{trts}. Length of \code{reps} has to match length of \code{trts}.
+#' @param nperlane An integer for number of samples per lane for sequencing. When \code{nperlane} is not provided, a number between 4-6 will be selected based on number of treatments to generate efficient design
+#' @param seed an integer initializing the random number generator. The default is \code{seed=1}
+#'
+#' @return input A list showing input parameters to the function
+#' @return Design A list with two elements: \code{design} and \code{BlocksEfficiency}. \code{design} is a data frame giving lane and adaptor assignment for treatment groups. \code{BlocksEfficiency} is a data frame giving block efficiencies (D-Efficiencies) for lane and adaptor.
+#' @return suggestedDesign When \code{nperlane} is \code{NULL} or same as the value would be selected to give better efficiencey, \code{suggestedDesign} is the same as \code{Design}. Otherwise, \code{suggestedDesign} uses different number of samples per lane from the input to give more efficient block design. \code{suggestedDesign} is a list with two elements: \code{design} and \code{BlocksEfficiency}. \code{design} is a data frame giving lane and adaptor assignment for treatment groups. \code{BlocksEfficiency} is a data frame giving block efficiencies (D-Efficiencies) for lane and adaptor.
+#'
+#'
+#' @keywords RNA-seq, statistical experimental design, block design, Illumina flow cell
+#'
+#' @export
+#'
+#' @examples
+#' gen.design(trts=letters[1:4], reps=rep(4,4), nperlane=NULL)
+#' gen.design(trts=letters[1:4], reps=c(2,4,4,2), nperlane=3)
+#' gen.design(trts=letters[1:6], reps=rep(3,6), nperlane=4)
+#'
+#' @import dplyr
+#' @importFrom blocksdesign blocks
+#'
+#'
+#'
+#'
+#'
+
+gen.design <- function(trts, reps, nperlane = NULL, seed = 1) {
+    if (class(trts) != "character") {
+        stop("trts needs to be a character vector for treatment groups")
+    }
+
+    if (!((class(reps) == "integer" & all(reps > 0)) | (class(reps) == "numeric" & all(reps >
+        0) & all(ceiling(reps) == floor(reps))))) {
+        stop("reps needs to be positive integers")
+    }
+
+    if (!is.null(nperlane)) {
+        if (!((class(nperlane) == "integer" & nperlane > 0) | (class(nperlane) == "numeric" &
+            nperlane > 0 & ceiling(nperlane) == floor(nperlane)))) {
+            stop("if nperlane is not NULL, it needs be a positive integer")
+        }
+    }
+
+    if (length(trts) != length(reps)) {
+        stop("trts and reps need to be the same length")
+    }
+
+
+    out <- list(input = list(trts = trts, nperlane = nperlane, reps = reps, seed = seed), Design = list(),
+        suggestedDesign = list())
+    ntrts <- length(trts)
+
+    # suggested design, basically suggesting nperlane
+    if (ntrts <= 6) {
+        suggested.nperlane <- switch(ntrts, 4, 4, 3, 4, 5, 6)
+    } else {
+        # ntrts>6
+        suggested.nperlane <- 4
+        if (ntrts%%5 == 0)
+            suggested.nperlane <- 5
+        if (ntrts%%3 == 0 & ntrts%%4 != 0)
+            suggested.nperlane <- 6
+    }
+
+    if ((sum(reps)%%suggested.nperlane) != 0) {
+        # not filing a whole lane, add samples belong to ' ' to fill
+        reps <- c(reps, (sum(reps)%%suggested.nperlane))
+        trts <- c(trts, " ")
+    }
+
+    suggested.des <- blocks(treatments = rep(1, length(reps)), replicates = reps, seed = seed,
+        rows = suggested.nperlane, columns = ceiling(sum(reps)/suggested.nperlane))
+    out$suggestedDesign$design <- suggested.des$Design %>% transmute(chip = 1, lane = gsub("Cols_",
+        "", Level_1.Cols) %>% as.integer, adp = gsub("Rows_", "", Level_1.Rows) %>% as.integer,
+        trt = trts[Treatments])
+
+    #this is for one chip design, need to modify for multiple chips
+    out$suggestedDesign$BlocksEfficiency <- suggested.des$BlocksEfficiency[1:2,3:4]
+    names( out$suggestedDesign$BlocksEfficiency) <- c("numBlocks","Efficiency")
+    row.names( out$suggestedDesign$BlocksEfficiency) <- c("Adaptors","Lanes")
+
+
+    if (!is.null(nperlane)) {
+
+        if ((sum(reps)%%nperlane) != 0) {
+            # not filing a whole lane, add samples belong to ' ' to fill
+            reps <- c(reps, (sum(reps)%%nperlane))
+            trts <- c(trts, " ")
+        }
+        des <- blocks(treatments = rep(1, length(reps)), replicates = reps, seed = seed, rows = nperlane,
+            columns = ceiling(sum(reps)/nperlane))
+        out$Design$design <- des$Design %>% transmute(chip = 1, lane = gsub("Cols_", "",
+            Level_1.Cols) %>% as.integer, adp = gsub("Rows_", "", Level_1.Rows) %>% as.integer,
+            trt = trts[Treatments])
+        out$Design$BlocksEfficiency <- des$BlocksEfficiency[1:2,3:4]
+        names(out$Design$BlocksEfficiency) <- c("numBlocks","Efficiency")
+        row.names(out$Design$BlocksEfficiency) <- c("Adaptors","Lanes")
+    } else {
+        # nperlane is null
+
+        out$Design <- out$suggestedDesign  #when nperlane is NULL, optimal design is the same as suggested design
+    }
+    out  #output
+}
+
+
+
+
---
+++ R/plot.design.R
@@ -0,0 +1,86 @@
+#' Field map of RNA-seq experimental design
+#'
+#' Plot the RNA-seq experimental design in the format of Illumina 8-lane flow cell
+#'
+#'
+#' @param designtable a data frame for lane and adaptor assignment. Use output from \code{gen.design}.It should have 4 columns with column names \code{chip, lane, adp, trt}.
+#'
+#' @return None
+#'
+#'
+#' @keywords RNA-seq, statistical experimental design, block design, Illumina flow cell
+#'
+#' @export
+#'
+#' @examples
+#' des <- gen.design(trts=letters[1:6], reps=rep(3,6), nperlane=4)
+#' plot.design (des$Design$design)
+#' plot.design (des$suggestedDesign$design)
+#'
+#' @import dplyr
+#' @import ggplot2
+#' @importFrom RColorBrewer brewer.pal
+#' @importFrom grDevices colorRampPalette
+#'
+#'
+#'
+#'
+
+
+plot.design <- function(designtable) {
+  stopifnot(class(designtable) == "data.frame")
+  inputs <- names(designtable)
+  if (!("lane" %in% inputs)) {
+    stop("need lane assignment in design")
+  }
+  if (sum(designtable$lane %in% 1:8) != length(designtable$lane)) {
+    stop("lane needs to be 1 to 8")
+  }
+  if (!("adp" %in% inputs)) {
+    stop("need adaptor assignment in design")
+  }
+  if (!("chip" %in% inputs)) {
+    stop("need chip assignment in design")
+  }
+
+  nadp <- max(designtable$adp)
+  lanelabel <- data.frame(lane = 1:8, adp = nadp + 1.2, label = paste0("lane", 1:8))
+  adplabel <- data.frame(lane = rep(0, nadp), adp = (1:nadp) + 0.5,
+                         label = paste(paste0("sample", 1:nadp),paste0("adp", 1:nadp),sep=":"))
+
+  ntrt <- unique(designtable$trt) %>% length
+
+  if (ntrt <= 12) {
+    if (" " %in% designtable$trt) {
+      colors <- c("grey100", brewer.pal(ntrt - 1, "Paired"))
+    } else {
+      colors <- brewer.pal(ntrt, "Paired")
+    }
+  } else {
+    if (" " %in% designtable$trt) {
+      colors <- c("grey100", colorRampPalette(rev(brewer.pal(n = 7, name = "RdYlBu")))(ntrt - 1))
+    } else {
+      colors <- colorRampPalette(rev(brewer.pal(n = 7, name = "RdYlBu")))(ntrt)
+    }
+  }
+
+  p <- ggplot(designtable, aes(xmin = lane - 0.5, xmax = lane + 0.5, ymin = adp, ymax = adp + 1)) +
+    xlim(-0.5, 8.5) + ylim(0, nadp + 2) +
+    geom_rect(aes(fill = trt), colour = "grey50") +
+    scale_fill_manual(values = colors) +
+    geom_text(aes(x = lane, y = adp + 0.5, label = trt), show.legend = FALSE) +
+    geom_text(data = lanelabel,
+              aes(x = lane, y = adp, label = label)) +
+    geom_text(data = adplabel, aes(x = lane, y = adp, label = label)) +
+    theme_bw() +
+    theme(panel.border = element_blank(), panel.grid.major.x = element_blank(), panel.grid.minor.x = element_blank(),
+          panel.grid.major.y = element_blank(), panel.grid.minor.y = element_blank(), legend.position = "none",
+          axis.title.x = element_blank(), axis.text.x = element_blank(), axis.ticks.x = element_blank(),
+          axis.title.y = element_blank(), axis.text.y = element_blank(), axis.ticks.y = element_blank())
+
+  if (length(unique(designtable$chip)) == 1) {
+    p
+  } else {
+    p + facet_grid(chip ~ .)
+  }
+}
